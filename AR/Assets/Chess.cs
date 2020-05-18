7760f438d094064671639534c3a180e811cd1777
dasna111 <56249201+dasna111@users.noreply.github.com>
2020-05-18 13:20:22 +0200

    Game

---
+++ AR/Assets/Chess.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chess : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Queen.transform.position = transform.position(QueenStartX, QueenStartY, QueenStartZ);
+        Rook.transform.position = transform.position(RookStartX, RookStartY, RookStartZ);
+        Knight.transform.position = transform.position(RookStartX, RookStartY, RookStartZ);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ AR/Assets/Main.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZXing;
+using ZXing.QrCode;
+
+private WebCamTexture camTexture;
+private Rect screenRect;
+
+public class Main : MonoBehaviour
+{
+    #region QR
+
+    void Start()
+    {
+        screenRect = new Rect(0, 0, Screen.width, Screen.height);
+        camTexture = new WebCamTexture();
+        camTexture.requestedHeight = Screen.height;
+        camTexture.requestedWidth = Screen.width;
+        if (camTexture != null)
+        {
+            camTexture.Play();
+        }
+    }
+
+    void OnGUI()
+    {
+        // drawing the camera on screen
+        GUI.DrawTexture(screenRect, camTexture, ScaleMode.ScaleToFit);
+        // do the reading — you might want to attempt to read less often than you draw on the screen for performance sake
+        try
+        {
+            IBarcodeReader barcodeReader = new BarcodeReader();
+            // decode the current frame
+            var result = barcodeReader.Decode(camTexture.GetPixels32(),
+              camTexture.width, camTexture.height);
+            if (result != null)
+            {
+                Debug.Log("DECODED TEXT FROM QR: " + result.Text);
+            }
+        }
+        catch (Exception ex) { Debug.LogWarning(ex.Message); }
+    }
+    private static Color32[] Encode(string textForEncoding, int width, int height) // reading and writing QR
+    {
+        var writer = new BarcodeWriter
+        {
+            Format = BarcodeFormat.QR_CODE,
+            Options = new QrCodeEncodingOptions
+            {
+                Height = height,
+                Width = width
+            }
+        };
+        return writer.Write(textForEncoding);
+    }
+    public Texture2D generateQR(string text) // generate a 2D texture and display in in the GUI
+    {
+        var encoded = new Texture2D(256, 256);
+        var color32 = Encode(text, encoded.width, encoded.height);
+        encoded.SetPixels32(color32);
+        encoded.Apply();
+        return encoded;
+    }
+
+    #endregion QR
+
+    #region Location
+
+    IEnumerator CheckLocation()
+    {
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+            yield break;
+
+        // Start service before querying location
+        Input.location.Start();
+
+        // Wait until service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait < 1)
+        {
+            print("Timed out");
+            yield break;
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            print("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            // Access granted and location value could be retrieved
+            print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+        }
+
+        /*
+        // Stop service if there is no need to query location updates continuously
+        Input.location.Stop();
+        */
+    }
+
+    #endregion Location
+}
